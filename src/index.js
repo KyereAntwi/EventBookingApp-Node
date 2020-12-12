8882ffaaa7c040cbad1bde089361a5534e7383ec
Joseph Kyere Antwi <antiwkaykyere@gmail.com>
2020-12-12 02:03:51 +0000

    Added save settings

--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const fileUpload = require('express-fileupload');
-const eventRoutes = require('./controllers/eventControllers');
-const organizationsRoutes = require('./controllers/organizationController');
-const authRoutes = require('./controllers/authController');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const fileUpload = require("express-fileupload");
+const eventRoutes = require("./controllers/eventControllers");
+const organizationsRoutes = require("./controllers/organizationController");
+const authRoutes = require("./controllers/authController");
 
 const app = express();
 dotenv.config();
 
-mongoose.connect('mongodb://localhost:27017/event-booking-node', 
-{useNewUrlParser: true},
-() => console.log('Mongo db is connected'));
+mongoose.connect(
+  "mongodb://localhost:27017/event-booking-node",
+  { useNewUrlParser: true },
+  () => console.log("Mongo db is connected")
+);
 
 // middlewares
 app.use(express.json());
 app.use(fileUpload());
- 
+
 // controller routes
-app.use('/api/v1', eventRoutes);
-app.use('/api/v1', organizationsRoutes);
-app.use('/api/v1', authRoutes);
+app.use("/api/v1", eventRoutes);
+app.use("/api/v1", organizationsRoutes);
+app.use("/api/v1", authRoutes);
 
-
-app.listen(process.env.PORT || 3000, () => console.log('App is running'));
+app.listen(process.env.PORT || 3000, () => console.log("App is running"));
